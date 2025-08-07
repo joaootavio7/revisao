575ad54029bb01284a6a3f299781c1e976558907
joaootavio7 <ojoao21513@gmail.com>
2025-08-07 16:33:44 -0300

    Delete primeiralista1.js

--- primeiralista1.js
+++
@@ -1,7 +0,0 @@
-/*Faça um programa para ler dois valores inteiros, e depois mostrar na tela a soma desses números com uma
-mensagem explicativa, conforme exemplos.*/
-var teclado = require("prompt-sync")();
-var n1 = parseInt(teclado("Digite o valor do primeiro n\u00FAmero:"));
-var n2 = parseInt(teclado("Digite o valor do segundo n\u00FAmero: "));
-var soma = n1 + n2;
-console.log("A soma deu ".concat(soma));
