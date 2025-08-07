f5b5ba75051bd89d09cc3d1c449ddeaacc0fb394
joaootavio7 <ojoao21513@gmail.com>
2025-08-07 16:32:22 -0300

    Delete quartalista6.js

--- quartalista6.js
+++
@@ -1,7 +0,0 @@
-var teclado = require("prompt-sync")();
-var n = parseInt(teclado("Digite o n\u00FAmero que deseja ver os divisores: "));
-for (var i = 1; i <= n; i++) {
-    if (n % i == 0) {
-        console.log(i);
-    }
-}
