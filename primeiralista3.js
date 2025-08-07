11773ad7438570cff76a2daed8dfda2b82602075
joaootavio7 <ojoao21513@gmail.com>
2025-08-07 16:33:13 -0300

    Delete primeiralista3.js

--- primeiralista3.js
+++
@@ -1,9 +0,0 @@
-/*Fazer um programa para ler quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto
-de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D).*/
-var teclado = require("prompt-sync")();
-var a = parseInt(teclado("Digite o valor de A:"));
-var b = parseInt(teclado("Digite o valor de B: "));
-var c = parseInt(teclado("Digite o valor de C:"));
-var d = parseInt(teclado("Digite o valor de D: "));
-var diferenca = a * b - c * d;
-console.log("A diferen\u00E7a dos n\u00FAmeros \u00E9: ".concat(diferenca));
